f9396c9b97b38e73e616ce225d70dbf0819c3aaf
Agung Gumelar <gunghello@gmail.com>
2025-05-16 17:09:54 +0700

    tambah video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,12 @@ export default function Home() {
       <h1>Bapa Cece</h1>
       <Image src="/bapa.jpg" width={1000} height={1000} alt="bapa" />
       <p>Bapa nuju di bogor tangal 16 mei 2025</p>
+      <p>bapa hoyong terang naon sih coding teh</p>
+
+      <video width="600" controls>
+        <source src="/video.mp4" type="video/mp4" />
+        Browser kamu tidak mendukung video tag.
+      </video>
     </>
   );
 }

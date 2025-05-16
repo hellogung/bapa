59c5d246b801b36c96e6114e87a23c0bf1c3965b
Agung Gumelar <gunghello@gmail.com>
2025-05-16 16:59:46 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['http://172.24.128.1:3000']
+
 };
 
 export default nextConfig;
